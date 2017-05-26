2668f164586de426d818396231cbbff5ccff0fe2
daviddansereau <daviddansereau@Davids-MacBook-Pro-3.local>
2017-05-26 15:03:53 -0500

    4

--- functions/postMessage.js
+++ functions/postMessage.js
@@ -1,75 +1,36 @@
-// Processes incoming messages for Zombie chat service
-var aws = require('aws-sdk');
-var ddb;
-var querystring = require('querystring');
+// create an IAM Lambda role with access to dynamodb
+// Launch Lambda in the same region as your dynamodb region
+// (here: us-east-1)
+// dynamodb table with hash key = user and range key = datetime
 
-var theContext;
-var message;
-var from;
-var numMedia;
-var mediaURL;
-var timestamp;
-var channel = 'default';
-
+console.log('Loading event');
+var AWS = require('aws-sdk');
+var dynamodb = new AWS.DynamoDB({apiVersion: '2012-08-10'});
 
 exports.handler = function(event, context) {
-    init(context);
-    theContext = context;
-
-    if(event.message == null || event.message == 'null' || event.name == null || event.name == 'null') {
-        return context.fail("Message and Name cannot be null");
-    } else {
-        message = event.message;
-        from = event.name;
-    }
-
-    if (event.timestamp == null || event.timestamp == 'null') {
-        event.timestamp = "" + new Date().getTime();
-        timestamp = event.timestamp;
-    }
-
-    /**
-     * For Debubugging input params to the lambda function
-    console.log('Message: ' + message);
-    console.log('Sender: ' + from);
-    console.log('Channel: ' + channel);
-    */
-
-    var DDBparams = {
-        "Item": {
-            "channel":{"S":channel},
-            "message":{"S":message},
-            "timestamp":{"N":timestamp},
-            "name":{"S":from}
-        }
-    };
-
-    dynamoPut(DDBparams);
-};
-
-function init(context) {
-  if(!ddb) {
-    console.log("Initializing DynamoDB client.");
-    var stackName = context.functionName.split('-z0mb1es-')[0];
-    var stackRegion = context.functionName.split('-WriteMessagesToDynamoDB-')[1];
-    ddb = new aws.DynamoDB({
-      region: stackRegion,
-      params: { TableName: stackName + "-messages" }
+    console.log(JSON.stringify(event, null, '  '));
+    dynamodb.listTables(function(err, data) {
+      console.log(JSON.stringify(data, null, '  '));
     });
-  }
-}
 
-function dynamoPut(params){
-    console.log("Putting item into DynamoDB");
-    ddb.putItem(params, dynamoCallback);
-}
+    var tableName = "chat";
+    var datetime = new Date().getTime().toString();
 
-function dynamoCallback(err, response) {
-  if (err) {
-      console.log('error' + err, err.stack); // an error occurred
-      return theContext.fail("There was an error.");
-  } else {
-      console.log('Success: ' + '{Sender: ' + from + ', ' + 'Message: ' + message + '}');
-      return theContext.succeed();
-  }
-}
+    dynamodb.putItem({
+        "TableName": ActivitiesTable,
+        "Item" : {
+            "activityDate": {"S": datetime },
+            "activitySource": {"S": ""},
+            "activityWith": {"S": ""},
+            "activityType": {"S": ""}
+        }
+    }, function(err, data) {
+        if (err) {
+            context.done('error','putting item into dynamodb failed: '+err);
+        }
+        else {
+            console.log('great success: '+JSON.stringify(data, null, '  '));
+            context.done('K THX BY');
+        }
+    });
+};
