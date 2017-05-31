532cfbb3e418142084d43186c9a806ed5dc7c957
daviddansereau <daviddansereau@Davids-MacBook-Pro-3.local>
2017-05-31 15:58:31 -0500

    7

--- functions/postMessage.js
+++ functions/postMessage.js
@@ -1,12 +1,9 @@
-console.log('Loading event');
 var AWS = require('aws-sdk');
 var docs = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
-//const docs = new AWS.DynamoDB.DocumentClient;
 const dynamo = new AWS.DynamoDB();
-//const docs = dynamo.DocumentClient();
-//var docs = new AWS.DynamoDB.DocumentClient();
 
-exports.handler = function(event, context) {
+
+exports.handler = function(event, context, callback) {
     console.log(JSON.stringify(event, null, '  '));
     dynamo.listTables(function(err, data) {
       console.log(JSON.stringify(data, null, '  '));
@@ -15,22 +12,25 @@ exports.handler = function(event, context) {
     var tableName = "ActivitiesTable";
     var datetime = new Date().getTime().toString();
     var body = JSON.parse(event.body);
+    var timestamp = "" + new Date().getTime().toString();
 
     //dynamo.DocClient.put({
     docs.put({
         "TableName": "RBActivities",
         "Item" : {
-            "activityDate": {"S": body.activityDate },
-            "activitySource": {"S": body.activitySource},
-            "activityWith": {"S": body.activityWith},
-            "activityType": {"S": body.activityType}
+            "activityId": timestamp,
+            "activityDate": body.activityDate,
+            "activitySource": body.activitySource,
+            "activityWith": body.activityWith,
+            "activityType": body.activityType
         }
     }, function(err, data) {
         if (err) {
-            context.done('error','putting item into dynamodb failed: '+err);
+            callback(err + " " + body.timestamp, null);
         }
         else {
             console.log('great success: '+JSON.stringify(data, null, '  '));
+            callback(null, 'success');
         }
     });
 };
