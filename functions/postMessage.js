0c97acbde1f759e216c8a8a295843e72321edeb8
daviddansereau <daviddansereau@Davids-MacBook-Pro-3.local>
2017-06-02 14:40:11 -0500

    8

--- functions/postMessage.js
+++ functions/postMessage.js
@@ -1,7 +1,7 @@
 var AWS = require('aws-sdk');
 var docs = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
+var token = "uk4pWPlX1Q6LqelLK4b0q8QY";
 const dynamo = new AWS.DynamoDB();
-
 
 exports.handler = function(event, context, callback) {
     console.log(JSON.stringify(event, null, '  '));
@@ -10,19 +10,25 @@ exports.handler = function(event, context, callback) {
     });
 
     var tableName = "ActivitiesTable";
-    var datetime = new Date().getTime().toString();
     var body = JSON.parse(event.body);
-    var timestamp = "" + new Date().getTime().toString();
+    var timeStamp = "" + new Date().getTime().toString();
+    var activityDate = new Date().toString();
+    //var requestToken = params.token;
 
-    //dynamo.DocClient.put({
+    /*if (requestToken != token) {
+        console.error("Request token (" + requestToken + ") does not match exptected token for Slack");
+        context.fail("Invalid request token");
+    }*/
+
     docs.put({
         "TableName": "RBActivities",
         "Item" : {
-            "activityId": timestamp,
-            "activityDate": body.activityDate,
+            "activityId": timeStamp,
+            "activityDate": activityDate,
             "activitySource": body.activitySource,
             "activityWith": body.activityWith,
-            "activityType": body.activityType
+            "activityType": body.activityType,
+            "company": body.company
         }
     }, function(err, data) {
         if (err) {
