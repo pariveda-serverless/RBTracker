7a4ee9d842daa936a7a9a3090ebc79a79bf32fd3
daviddansereau <daviddansereau@Davids-MacBook-Pro-3.local>
2017-06-26 14:24:26 -0500

    whatevs 4

--- activities/dynoToCsv.js
+++ activities/dynoToCsv.js
@@ -1,20 +1,28 @@
 var AWS = require('aws-sdk');
 var docs = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
-var dynamo = new AWS.DynamoDB();var json2csv = require('json2csv');
+var dynamo = new AWS.DynamoDB();
+var json2csv = require('json2csv');
 
-var params = {
-  TableName : 'RBActivities'
-};
+exports.handler = function(event, context, callback) {
 
-var documentClient = new AWS.DynamoDB.DocumentClient();
+  var fields = ['activityDate', 'Company', 'Event', 'Who', 'With'];
+  var params = {
+          TableName : "RBActivities"
+      };
 
-var fields = ['activityDate', 'Company', 'Event', 'Who', 'With'];
-var allActivities = documentClient.query(params, function(err, data) {
-  if (err) console.log(err);
-  else console.log(data);
-});
 
-var csv = json2csv({ data: allActivities, fields: fields });
+  docs.scan(params, function(err, data) {
+    if (err) console.log(err);
+    else {
+        /*console.log(data.Items[0]);
+        console.log(data.Items[1]);*/
 
-console.log(csv);
- //
+        data.Items.forEach(function(element){
+          console.log(element);
+        });
+
+        var csv = json2csv({ data: data.Items[0], fields: fields });
+        console.log(csv);
+      }
+   });
+}
